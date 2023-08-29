68228deb369161033e36d1411e8989e89b617fe5
Väinö Rantanen <vaino01@hotmail.fi>
2023-08-29 21:26:36 +0300

    added logs

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -4,13 +4,18 @@ const PORT = process.env.PORT || 3001
 
 let MONGODB_URI
 
+console.log(process.env.MONGODB_TEST_URI, PORT)
+
 if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config()
   MONGODB_URI = process.env.MONGODB_TEST_URI
 } else if (process.env.NODE_ENV === 'development') {
   MONGODB_URI = process.env.MONGODB_DEV_URI
 } else {
   MONGODB_URI = process.env.MONGODB_URI
 }
+
+console.log(MONGODB_URI)
 
 module.exports = {
   MONGODB_URI,
