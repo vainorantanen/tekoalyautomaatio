3a9ada17831e70b37cfe6f40e5c9ee7ca4bcf49b
Väinö Rantanen <vaino01@hotmail.fi>
2023-08-29 21:40:11 +0300

    fixing pipeline

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -7,11 +7,13 @@ let MONGODB_URI
 console.log(process.env.MONGODB_TEST_URI, PORT)
 
 if (process.env.NODE_ENV === 'test') {
-  require('dotenv').config()
+  console.log('running on test')
   MONGODB_URI = process.env.MONGODB_TEST_URI
 } else if (process.env.NODE_ENV === 'development') {
+  console.log('running on dev')
   MONGODB_URI = process.env.MONGODB_DEV_URI
 } else {
+  console.log('running on prod')
   MONGODB_URI = process.env.MONGODB_URI
 }
 
