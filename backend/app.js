c0fea9feacbc4812741ce1eafb045dfe29d5bb5e
Väinö Rantanen <vaino01@hotmail.fi>
2023-08-29 21:46:15 +0300

    poistettu turhia console logeja

--- backend/app.js
+++ backend/app.js
@@ -25,10 +25,7 @@ const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
 
-
 logger.info('connecting to', config.MONGODB_URI)
-
-console.log('connecting to', config.MONGODB_URI)
 
 mongoose
   .connect(config.MONGODB_URI)
--- backend/utils/config.js
+++ backend/utils/config.js
@@ -4,20 +4,13 @@ const PORT = process.env.PORT || 3001
 
 let MONGODB_URI
 
-console.log(process.env.MONGODB_TEST_URI, PORT)
-
 if (process.env.NODE_ENV === 'test') {
-  console.log('running on test')
   MONGODB_URI = process.env.MONGODB_TEST_URI
 } else if (process.env.NODE_ENV === 'development') {
-  console.log('running on dev')
   MONGODB_URI = process.env.MONGODB_DEV_URI
 } else {
-  console.log('running on prod')
   MONGODB_URI = process.env.MONGODB_URI
 }
-
-console.log(MONGODB_URI)
 
 module.exports = {
   MONGODB_URI,
