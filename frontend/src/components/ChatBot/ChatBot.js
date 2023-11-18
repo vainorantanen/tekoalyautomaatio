2457f6efcc37ea18e7205f727e0bc882a3228362
Väinö Rantanen <vaino01@hotmail.fi>
2023-11-18 22:04:54 +0200

    tehty toggle ominaisuus chatbotille

--- frontend/src/components/ChatBot/ChatBot.js
+++ frontend/src/components/ChatBot/ChatBot.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Chatbot } from "react-chatbot-kit";
 
 import config from './chatbotConfig';
@@ -6,17 +6,39 @@ import MessageParser from './MessageParser';
 import ActionProvider from './ActionProvider';
 
 import './chatbot.css'; 
+import { IconButton } from '@mui/material';
+import ChatIcon from '@mui/icons-material/Chat';
+
 
 const ChatbotComponent = () => {
+  const [showChat, setShowChat] = useState(false);
+
+  const toggleChat = () => {
+    setShowChat(!showChat);
+  };
 
   return (
     <div className="chatbot-container">
-      <div className="chatbot-wrapper">
-        <Chatbot
-          config={config}
-          messageParser={MessageParser}
-          actionProvider={ActionProvider}
-        />
+      {showChat && (
+        <div className="chatbot-wrapper">
+          <Chatbot
+            config={config}
+            messageParser={MessageParser}
+            actionProvider={ActionProvider}
+          />
+        </div>
+      )}
+
+      <div style={{ 
+        position: 'fixed',
+        bottom: '20px',
+        right: '20px',
+        zIndex: 999
+       }}>
+        <IconButton onClick={toggleChat}>
+          <ChatIcon sx={{ color: 'white', backgroundColor: 'blue',
+        padding: '1rem', borderRadius: '0.5rem' }}/>
+        </IconButton>
       </div>
     </div>
   );
